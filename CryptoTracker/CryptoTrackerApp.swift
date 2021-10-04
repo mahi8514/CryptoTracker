967127b9a23f07eb9b61d359247797b0a32dd8fb
Mahin Ibrahim <mahin8514@gmail.com>
2021-10-04 18:46:06 +0400

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Mahin Ibrahim on 04/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
